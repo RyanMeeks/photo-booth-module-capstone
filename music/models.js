5fe7f540d335c53d232dc240525b238eaccc367c
Ryan Meeks <ryan_meeks613@gmail.com>
2018-06-07 14:51:08 -0700

    Add music chart and song models

--- music/models.js
+++ music/models.js
@@ -1,33 +1,51 @@
 'use strict';
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
  
+
+const songSchema = mongoose.Schema({
+    position: {type: Number, required: false},
+    artist: String,
+    title: String
+});
+
+
 const musicChartSchema = mongoose.Schema({
     chart: {type: String, required: true},
-    list: [{
-        position: Number,
-        artist: String,
-        title: String
-    }]
+    songs: [{type: Schema.Types.ObjectId, ref: 'Song'}]
+});
+
+
+
+const musicListSchema = mongoose.Schema({
+    listName: {type: String, required: true},
+    songs: [{type: Schema.Types.ObjectId, ref: 'Song'}]
 })
 
-musicChartSchema.virtual('artistTitleString').get(function() {
-    return `${this.list.artist} - ${this.list.title}`;
-});
+//[{type: Schema.Types.ObjectId, ref: 'Song'}]
 
-musicChartSchema.virtual('rank').get(function() {
-    const rankSongs = this.list.sort((a, b) => {return b.postion - a.position;})[0] || {};
-    return rankSongs.position;
-});
 
-musicChartSchema.methods.serialize = function() {
-    return {
-        position: this.position,
-        artist: this.artist,
-        title: this.title
-    };
-};
+// musicChartSchema.virtual('artistTitleString').get(function() {
+//     return `${this.list.artist} - ${this.list.title}`;
+// });
 
-const MusicCharts = mongoose.model('MusicCharts', musicChartSchema);
+// musicChartSchema.virtual('rank').get(function() {
+//     const rankSongs = this.list.sort((a, b) => {return b.postion - a.position;})[0] || {};
+//     return rankSongs.position;
+// });
 
-module.exports = {MusicCharts};
+// musicChartSchema.methods.serialize = function() {
+//     return {
+//         position: this.position,
+//         artist: this.artist,
+//         title: this.title
+//     };
+// };
+
+
+const Song = mongoose.model('Song', songSchema);
+const MusicChart = mongoose.model('MusicChart', musicChartSchema);
+const MusicList = mongoose.model('MusicList', musicListSchema)
+
+module.exports = { MusicChart, Song, MusicList };
