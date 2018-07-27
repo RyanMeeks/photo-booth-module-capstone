12bf827a31cd073630cb207240f5ce33ef91559e
Ryan MeeksLAP <ryanmeeks613@gmail.com>
2018-07-27 12:52:14 -0700

    add mlab/heroku url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/event-flow-pro';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://heroku_7qn2gxzk:kcv2h487ipgeui3ahk2ua4o4vi@ds239940.mlab.com:39940/heroku_7qn2gxzk';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-event-flow-pro';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
