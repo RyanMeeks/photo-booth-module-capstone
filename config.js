06fc93780fa286537f471171e37d39f6ba1600ed
Ryan MeeksLAP <ryanmeeks613@gmail.com>
2018-07-27 13:53:21 -0700

    add links for databases (corrected)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://heroku_7qn2gxzk:kcv2h487ipgeui3ahk2ua4o4vi@ds239940.mlab.com:39940/heroku_7qn2gxzk';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://heroku_7qn2gxzk:kcv2h487ipgeui3ahk2ua4o4vi@ds239940.mlab.com:39940/heroku_7qn2gxzk';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://ryanm613:kgb00312@ds149279.mlab.com:49279/record-crate';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://ryanm614:kgb00312@ds149279.mlab.com:49279/record-crate';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
