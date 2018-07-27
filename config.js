8d56b32493192f9cc6c9fd984ed71e136484aed0
Ryan MeeksLAP <ryanmeeks613@gmail.com>
2018-07-27 12:54:08 -0700

    add test url to config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://heroku_7qn2gxzk:kcv2h487ipgeui3ahk2ua4o4vi@ds239940.mlab.com:39940/heroku_7qn2gxzk';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-event-flow-pro';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://heroku_7qn2gxzk:kcv2h487ipgeui3ahk2ua4o4vi@ds239940.mlab.com:39940/heroku_7qn2gxzk';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
